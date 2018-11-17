f673ff2a055f8fb38677055af2696bcc0cd4ed69
Neethu Thomson <n***t@gmail.com>
2018-11-17 17:58:07 -0500

    interim submission

--- spec/menu_controller_spec.js
+++ spec/menu_controller_spec.js
@@ -1,17 +1,15 @@
 const MenuController = require('../controllers/menuController');
 
-
 describe('menuController', () => {
+
   beforeEach(() => {
-    menu = new MenuController();
+    this.menu = new MenuController();
   })
-  describe('getContactCount()', () => {
-    it('should return 0 when no contacts are in the book', () => {
-      expect(menu.getContactCount()).toBe(0);
-    });
-    it('should return 1 when there is exactly one contact in the book', () => {
-      menu.contacts.push('Bob');
-      expect(menu.getContactCount()).toBe(1);
+
+  describe('#remindMe()', () =>{
+    it('should always return "learning is a lifelong pursuit."', () => {
+      expect(this.menu.remindMe()).toBe('Learning is a life-long pursuit.');
     });
   })
+
 })
